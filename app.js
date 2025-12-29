032c104b08a74ba66abe2379c5fb146d709d8804
AymaneBoutahar <aymane.boutahar@student.ehb.be>
2025-12-29 19:43:03 +0100

    user can register WIP

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const cors = require("cors")
+const connectDB = require("./db")
+
 const app = express()
 const port = 3000
+
+
 
 
 app.use(express.json());
@@ -17,7 +21,19 @@ app.post("/login", (req, res) => { //receive login data from front-end
 
 });
 
+
+app.post("/register", (req, res) =>{
+  console.log("BODY:", req.body);
+  
+  const {email, password} = req.body; //destructures everything
+  console.log("EMAIL:", email);
+  console.log("PASSWORD:", password);
+  res.send({message: "Registering complete!"})
+});
+
+
+
 //starts up the server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
