652100d8ff5499fb5e7462bb52c594d63febd28e
AymaneBoutahar <aymane.boutahar@student.ehb.be>
2025-12-30 17:52:17 +0100

    register + login connected and working to MongoDB

--- app.js
+++ app.js
@@ -17,29 +17,52 @@ app.use(cors());
 connectDB();
 
 ///////////////////////LOGIN/////////////////////////
-app.post("/login", (req, res) => { //receive login data from front-end
+app.post("/login", async (req, res) => { //receive login data from front-end
     const {user, password} = req.body; //destructures username and password
-  if (user == "Aymane" && password == "web2") {
-    res.send({message: "Login succesful!"});
-  } else { 
-    res.status(401).send({message: "Error: wrong credentials."})
-  };
+  if (!user || !password) {
+    return res.status(400).send({message: "Please fill in both fields"}) //checks if every field is filled in
+  } 
+    try {
+      const db = getDB(); //user the existing DB connection
+      const usersCollection = db.collection("users"); //selects the users collection
+      const userExists = await usersCollection.findOne({user}); //checks if a user has the same name
 
+      if (!userExists) {
+        return res.status(409).send({message: "This user already exists"}); //this message plays if theres a user with the inputted name
+    }
+
+    const correctPassword = await bcrypt.compare( // compares the passwords: 
+      password,
+      userExists.password
+    );
+
+    if (!correctPassword) {
+      return res.status(401).send({message: "Credentials don't match"}); //checks if password is correct
+    }
+    //logs you in and returns with what user you logged in with
+    res.send({message: "Succesfully logged in!",
+      user: userExists.user
+    })
+    
+  }catch(error) {
+      console.error(error);
+      res.status(500).send({message: "Failure to login"})
+    }
 });
 ///////////////////////LOGIN/////////////////////////
 
 ///////////////////////REGISTER/////////////////////////
 app.post("/register", async (req, res) =>{
   //console.log("BODY:", req.body);
-  const {name, password} = req.body; //destructures everything
+  const {user, password} = req.body; //destructures everything
 
-  if (!name || !password) { //checks if both fields aren't empty
+  if (!user || !password) { //checks if both fields aren't empty
     return res.status(400)
     .send({message: "Please fill in the required fields"});
   } try {
     const db = getDB(); //user the existing DB connection
     const usersCollection = db.collection("users"); //selects the users collection
-    const userExists = await usersCollection.findOne({name}); //checks if a user has the same name
+    const userExists = await usersCollection.findOne({user}); //checks if a user has the same name
 
     if (userExists) {
       return res.status(409).send({message: "This user already exists"}); //this message plays if theres a user with the inputted name
@@ -49,7 +72,7 @@ app.post("/register", async (req, res) =>{
 
     //safely stores user
     await usersCollection.insertOne({
-      name: name,
+      user: user,
       password: hashedpassword
     });
       res.send({message: "Registering user succesful!"});
